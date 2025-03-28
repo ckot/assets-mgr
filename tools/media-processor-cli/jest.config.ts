b3203a7ca0bb49a0771cf2e75806e289c1feba51
ckot <scott.t.silliman@gmail.com>
2025-03-28 00:46:44 -0400

    used nx 'move' to rename to media-processor-cli

--- tools/media-processor-cli/jest.config.ts
+++ tools/media-processor-cli/jest.config.ts
@@ -10,7 +10,7 @@ const swcJestConfig = JSON.parse(
 swcJestConfig.swcrc = false;
 
 export default {
-  displayName: 'image-processor-cli',
+  displayName: 'media-processor-cli',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
