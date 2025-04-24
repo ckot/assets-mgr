6c41ac2c6b3ab3b7a7248865480aa6a8b5646ac3
ckot <scott.t.silliman@gmail.com>
2025-04-23 21:37:27 -0400

    changes from running the @nx/react:application generator

---
+++ apps/media-mgr/app/app.tsx
@@ -0,0 +1,11 @@
+import './app.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="media-mgr" />
+    </div>
+  );
+}
+export default App;
